152255337efcd435a4e0654428ec63f2725562d1
Javier Viola <152718445+olaf-at-work@users.noreply.github.com>
2023-12-03 12:57:01 -0300

    refactor server

--- client.js
+++ client.js
@@ -1,55 +1,22 @@
 import fetch from 'node-fetch';
-import WebSocket from 'ws';
 
-
-// Your JSON data
 const jsonData = {
-    "settings": {
-        "provider": "native"
-    },
-    "relaychain": {
-        "chain": "rococo-local",
-        "default_command": "./bin/polkadot",
-        "nodes": [
-            {
-                "name": "alice",
-            },
-            {
-                "name": "bob",
-            }
-        ]
-    },
-    "parachains": [
-        {
-            "id": 2100,
-            "cumulus_based": true,
-            "chain": "local",
-            "add_to_genesis": false,
-            "onboard_as_parachain": false,
-            "collators": [
-                {
-                    "name": "parachain-collator01",
-                    "command": "./bin/parachain-template-node"
-                },
-                {
-                    "name": "parachain-collator02",
-                    "command": "./bin/parachain-template-node"
-                },
-                {
-                    "name": "parachain-collator03",
-                    "command": "./bin/parachain-template-node"
-                }
-            ]
-        }
-    ]
+    "para_id": 2100,
+    "template": "parity-extended-template",
+    "collators_count": 3,
+    "properties": {
+      "symbol": "PORT",
+      "ss58": 0,
+      "decimals" : 12
+    }
 };
 
 // URLs
 const apiUrl = 'http://localhost:4000/network';
-const wsUrl = 'ws://localhost:4000/';
 
 // Function to send the POST request
 function sendPostRequest() {
+    console.log(jsonData);
     fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -60,32 +27,11 @@ function sendPostRequest() {
         .then(response => response.json())
         .then(data => {
             console.log('POST request success:', JSON.stringify(data, null, 4));
-            connectWebSocket();
+
         })
         .catch(error => console.error('Error in POST request:', error));
 }
 
-// Function to establish the WebSocket connection
-function connectWebSocket() {
-    const ws = new WebSocket(wsUrl);
-
-    ws.on('open', () => {
-        console.log('WebSocket connection established');
-    });
-
-    ws.on('message', event => {
-        const message = event.toString();
-        console.log('Message from server:', message);
-    });
-
-    ws.on('close', () => {
-        console.log('WebSocket connection closed');
-    });
-
-    ws.on('error', error => {
-        console.error('WebSocket error:', error);
-    });
-}
 
 // Make the POST request
 sendPostRequest();
--- server.js
+++ server.js
@@ -1,134 +1,34 @@
 import Fastify from 'fastify';
-import { spawn } from 'child_process';
 import fs from 'fs/promises';
-import fastifyWebsocket from '@fastify/websocket';
-import { Network, start, Providers } from '@zombienet/orchestrator';
+import { start } from '@zombienet/orchestrator';
+import { sanitizeNetwork, generateConfig } from './zombieHelpers.js';
+
+// replace with template registry!
+const registry = {
+    "parity-extended-template" : {
+        "spec": "./spec/parity-extended-chainspec.json",
+        "bin": "./bin/parity-extended-node"
+    }
+};
+
 
 const fastify = Fastify({ logger: true });
-fastify.register(fastifyWebsocket);
-
-let outputBuffer = '';
-let connection = '';
-let connections = [];
-
-
-
-function parseZombienetOutput(output) {
-    const startIndex = output.indexOf("Network launched 🚀🚀");
-    const relevantOutput = output.substring(startIndex);
-    const lines = relevantOutput.split('\n');
-    const parsedData = [];
-    let currentEntry = {};
-
-    lines.forEach(line => {
-        if (line.includes('│ Name')) {
-            if (currentEntry.name) {
-                parsedData.push(currentEntry); // Save the previous entry
-                currentEntry = {}; // Reset for new entry
-            }
-            currentEntry.name = line.split('│')[2].trim();
-        } else if (line.includes('│ Direct Link')) {
-            currentEntry.directLink = line.split('│')[2].trim();
-        } else if (line.includes('│ Prometheus Link')) {
-            currentEntry.prometheusLink = line.split('│')[2].trim();
-        } else if (line.includes('│ Log Cmd')) {
-            currentEntry.logCmd = line.split('│')[3].trim();
-        }
-    });
-
-    if (currentEntry.name) {
-        parsedData.push(currentEntry); // Save the last entry
-    }
-
-    return parsedData;
-}
-
-fastify.register(async function (fastify) {
-    fastify.get('/', { websocket: true }, (connection, req) => {
-        console.log('WebSocket client connected');
-        connections.push(connection.socket);
-
-        connection.socket.on('message', message => {
-            console.log('Received message from client:', message);
-            connection.socket.send('hi from server');
-        });
-
-        connection.socket.on('close', () => {
-            console.log('WebSocket client disconnected');
-            connections = connections.filter(conn => conn !== connection.socket);
-        });
-    });
-});
-
 
 fastify.post('/network', async (request, reply) => {
-    const date = formatDate();
-    const networkTopology = request.body;
-    const formattedJson = JSON.stringify(networkTopology, null, 4);
-    await fs.writeFile('./zombienet-config/rococo-local-config_generated.json', formattedJson);
+    const template = registry[request.body.template];
+    if(! template) return reply.status(400).send({code: 400, msg: "invalid template"});
+
+    const requestTS = new Date().toISOString().split(".")[0].replaceAll(/[:-]/g,"");
+    const networkTopology = await generateConfig(request.body, template, requestTS);
+    await fs.writeFile(`./zombienet-config/config_${requestTS}_generated.json`, JSON.stringify(networkTopology, null, 4));
 
     // needs some validation here
-    const network = await start("ZOMBIENET_CREDENTIALS", networkTopology, {
+    const network = await start("",networkTopology, {
         spawnConcurrency: 5,
     });
 
-    // network should be stored globally
-    console.log(network);
-    console.log("\n\n");
-
     reply.send({ result: 'OK', network: sanitizeNetwork(network) });
 });
-
-// Broadcast the data to all connected clients
-function broadcastToClients(data) {
-    connections.forEach(socket => {
-        if (socket.readyState === 1) {
-            socket.send(data);
-        }
-    });
-}
-
-function sanitizeNetwork(network) {
-    const relay = network.relay.map(sanitizeNode)
-
-    const paras = Object.keys(network.paras).reduce((memo, paraId) => {
-        const nodes = network.paras[paraId].nodes.map(sanitizeNode);
-        memo[paraId] = nodes;
-        return memo;
-    }, {});
-
-    return {
-        ns: network.namespaces,
-        relay,
-        paras,
-    }
-}
-
-function sanitizeNode({name, wsUri, prometheusUri, multiAddress}) {
-    const parts = multiAddress.split("/");
-    return {
-        name,
-        wsUri,
-        prometheusUri,
-        p2pId: parts[parts.length -1]
-    }
-}
-
-function formatDate() {
-    const now = new Date();
-
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
-}
-
-
-
 
 fastify.listen({ port: 4000 }, err => {
     if (err) {
